a9a8ecbca64786a61e4f3415ec1d847b4852992d
daichiPon <nakamotodaich@icloud.com>
2024-10-14 20:58:58 +0900

    react-scriptを削除してexpoを入れた

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from"../App";
+import App from "../App";
 
-AppRegistry.registerComponent('QrcodeOrder', () => App);
-AppRegistry.runApplication('QrcodeOrder', {
-  initialProps: {},
-  rootTag: document.getElementById('root'),
-});
+AppRegistry.registerComponent('qrorder', () => App);
